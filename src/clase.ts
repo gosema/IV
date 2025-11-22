59cf53ddaed0309565d973a3b811d779ddca503d
J0RG170 <e.jorgelpz@go.ugr.es>
2025-11-22 18:14:39 +0100

    definicion para el objeto Clase
    Se aporta un codigo que consiste en el objeto clase. Esta preparado para contener los datos mencionados en la issue #16. Dichos datos son extraidos de la fuente de datos. Este objeto representa la entidad mínima del producto. Closes #16

---
+++ src/clase.ts
@@ -0,0 +1,15 @@
+class Clase {
+    grupo: string;
+    dia: string;
+    semestre: string;
+    horario_inicio: string;
+    horario_final: string;
+
+    constructor(grupo: string, dia: string, semestre: string, horario_inicio: string, horario_final: string) {
+        this.grupo = grupo;
+        this.dia = dia;
+        this.semestre = semestre;
+        this.horario_inicio = horario_inicio;
+        this.horario_final = horario_final;
+    }
+}
