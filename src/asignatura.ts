c7c73263980d32a5d8610bef2ed9d281f2acccb4
J0RG170 <e.jorgelpz@go.ugr.es>
2025-11-22 18:25:20 +0100

    identificación única de asignaturas
    las asignaturas deben poder identificarse inequívocamente asi como lo expresa la issue #23. Para ello se va a usar la URL. Las URL son únicas luego es un dato válido para funcionar como identificador.Closes #23.

--- src/asignatura.ts
+++ src/asignatura.ts
@@ -1,5 +1,8 @@
 // Clase que define la entidad
 class Asignatura {
+    // Sugerencia: Usar la URL de la asignatura en la web de la universidad como ID
+    // Dicha URL es única para cada asignatura.
+    // Ejemplo: "https://www.ugr.es/estudiantes/grados/grado-ingenieria-informatica/calculo"
     id: string;
     clasesPracticas: Map<string, Clase>;
     clasesTeoricas: Clase[];
