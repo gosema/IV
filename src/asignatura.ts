822e6b385477ef9a1ee1ddb9657cd2cf16b3d4a3
J0RG170 <e.jorgelpz@go.ugr.es>
2025-11-23 19:23:21 +0100

    fix al nombre del identificador
    Para justificarme en la elección de la URL como identificador quiero aclarar que los datos son obtenidos de una fuente que en este caso es una web. La universidad ya tiene organizadas las asignaturas y se accede a ellas a traves de la url. La url, además de ser única aporta información relevante sobre la asignatura que la convierte en identificable y diferenciable de las demás que puedan llamarse igual o parecido en otros grados. Lo mejor de todo es que ya está hecha, es un dato que se extrae. No es necesario realizar operaciones ni ningún tipo de agregación para identificar las asignaturas 1 a 1.Closes #23

--- src/asignatura.ts
+++ src/asignatura.ts
@@ -3,12 +3,12 @@ class Asignatura {
     // Sugerencia: Usar la URL de la asignatura en la web de la universidad como ID
     // Dicha URL es única para cada asignatura.
     // Ejemplo: "https://www.ugr.es/estudiantes/grados/grado-ingenieria-informatica/calculo"
-    id: string;
+    id_url: string;
     clasesPracticas: Map<string, Clase>;
     clasesTeoricas: Clase[];
 
-    constructor(id: string, clasesPracticas: [string, Clase][], clasesTeoricas: Clase[]) {
-        this.id = id;
+    constructor(id_url: string, clasesPracticas: [string, Clase][], clasesTeoricas: Clase[]) {
+        this.id_url = id_url;
         this.clasesPracticas = new Map(clasesPracticas);
         this.clasesTeoricas = clasesTeoricas;
     }
