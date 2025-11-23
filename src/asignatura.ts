69156771d6ae1ee8a67347f5a3dba15aa5c61bd5
J0RG170 <e.jorgelpz@go.ugr.es>
2025-11-23 19:24:19 +0100

    Fix al dato id_url
    Se elimina el comentario porque no es una buena práctica. Se mantiene el ejemplo porque creo que ayuda bastante a entender la decisión.

--- src/asignatura.ts
+++ src/asignatura.ts
@@ -1,7 +1,5 @@
 // Clase que define la entidad
 class Asignatura {
-    // Sugerencia: Usar la URL de la asignatura en la web de la universidad como ID
-    // Dicha URL es única para cada asignatura.
     // Ejemplo: "https://www.ugr.es/estudiantes/grados/grado-ingenieria-informatica/calculo"
     id_url: string;
     clasesPracticas: Map<string, Clase>;
