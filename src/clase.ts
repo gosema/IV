1eb97b4c6b68334b4c1870b7024071819acab39d
J0RG170 <e.jorgelpz@go.ugr.es>
2025-11-23 12:39:07 +0100

    valor obligado al semestre
    Creado un type Semestre para obligar a que el semestre solo pueda valer 1 o 2. Esto lo he hecho porque no es necesario que el semestre pueda tener ningun otro valor posible. De esta forma nos ahorramos la hacer comprobaciones extra en el constructor. Si se le trata de pasar un valor de semestre diferente a 1 o 2 saltará un error. Closes #25.

--- src/clase.ts
+++ src/clase.ts
@@ -1,11 +1,14 @@
+
+type Semestre= 1|2;
+
 class Clase {
     grupo: string;
     dia: string;
-    semestre: number;
+    semestre: Semestre;
     horario_inicio: string;
     horario_final: string;
 
-    constructor(grupo: string, dia: string, semestre: number, horario_inicio: string, horario_final: string) {
+    constructor(grupo: string, dia: string, semestre: Semestre, horario_inicio: string, horario_final: string) {
         this.grupo = grupo;
         this.dia = dia;
         this.semestre = semestre;
