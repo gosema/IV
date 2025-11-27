68ce19edd29bcc44aa4871cef64c67e4d2748c58
J0RG170 <e.jorgelpz@go.ugr.es>
2025-11-27 13:00:28 +0100

    fix a las nomenclaturas
    Dado que la mayoría de variables son camelCase continuaremos con este camino. La variable url soluciona el problema de nomenclatura de id_url. Closes #27

--- src/asignatura.ts
+++ src/asignatura.ts
@@ -1,12 +1,12 @@
 // Clase que define la entidad
 class Asignatura {
     // Ejemplo: "https://www.ugr.es/estudiantes/grados/grado-ingenieria-informatica/calculo"
-    id_url: string;
+    url: string;
     clasesPracticas: Map<string, Clase>;
     clasesTeoricas: Clase[];
 
-    constructor(id_url: string, clasesPracticas: [string, Clase][], clasesTeoricas: Clase[]) {
-        this.id_url = id_url;
+    constructor(url: string, clasesPracticas: [string, Clase][], clasesTeoricas: Clase[]) {
+        this.url = url;
         this.clasesPracticas = new Map(clasesPracticas);
         this.clasesTeoricas = clasesTeoricas;
     }
