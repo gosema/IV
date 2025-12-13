65b63e7cd11de760c241196455b9aece061b048d
gosema <gomezlaynez@gmail.com>
2025-12-13 15:45:39 +0100

    feat: #20 Importaciones correspondientes de tests
    Se importan las bibliotecas necesarias para utilizar deno
    como gestor de tests. Además de acceso a las constantes de
    errores cuando haya que comprobar que se lanzan y acceso a la función
    externa de scrapeo de semestres.

--- test/test.ts
+++ test/test.ts
@@ -1,1 +1,5 @@
 //Tests para validar la lógica de negocio
+import { describe, it } from "testing/bdd";
+import { expect } from "testing/expect";
+import { extraerSemestre } from "../src/scraper_semestre.ts"
+import { ERRORES } from '../src/errores.ts';
