fd176302049fb21e0a41e971ff69cd0a1976451e
gosema <gomezlaynez@gmail.com>
2025-12-13 15:34:29 +0100

    feat: #36 Mensajes de error sobre semestres
    Se escriben tres mensajes de error constantes para lanzar cuando
    se produzacan errores en la obtención de datos de semestre.
    Un error cuando no haya semestre en la web, otro error cuando el
    semestre este vacío y otro erro cuando el semestre esté en un formato
    no válido.

--- src/errores.ts
+++ src/errores.ts
@@ -1,5 +1,7 @@
 // archivo para los mensajes de error
 
 export const ERRORES = {
-
+    SEMESTRE_NO_DETECTADO: "No se ha podido encontrar el semestre en los datos.",
+    SEMESTRE_NO_VALIDO: "El semestre encontrado no pertenece a uno válido de la UGR.",
+    SEMESTRE_VACIO: "El semestre existe pero esta vacío."
 } as const;
