d2ddf5a24aecfc264bfe9e5e8544246a2cd42f1d
gosema <gomezlaynez@gmail.com>
2025-12-13 15:42:44 +0100

    fix: #36 Mensaje de error si no tenemos datos
    Para probar los test necesitamos datos reales obtenidos del html
    de la página de la UGR. Hace falta un error que sea lanzado cuando
    estos archivos de datos no se encuentren.

--- src/errores.ts
+++ src/errores.ts
@@ -3,5 +3,6 @@
 export const ERRORES = {
     SEMESTRE_NO_DETECTADO: "No se ha podido encontrar el semestre en los datos.",
     SEMESTRE_NO_VALIDO: "El semestre encontrado no pertenece a uno válido de la UGR.",
-    SEMESTRE_VACIO: "El semestre existe pero esta vacío."
+    SEMESTRE_VACIO: "El semestre existe pero esta vacío.",
+    ARCHIVO_NO_ENCONTRADO: "No se encontró el fichero de datos real para los tests."
 } as const;
