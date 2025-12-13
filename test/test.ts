e264571b6b34c953029e66b6c0fc9793d94541b0
gosema <gomezlaynez@gmail.com>
2025-12-13 15:56:10 +0100

    feat: #36 Comprobación de ficheros de datos
    Función privada para comprobar que los ficheros de datos con los que
     vamos a validar la lógica de negocio son encontrados. Si no, se lanza
     un error para advertir.

--- test/test.ts
+++ test/test.ts
@@ -7,3 +7,11 @@ import { ERRORES } from '../src/errores.ts';
 const pathCalculo = "./data/Cálculo _ Universidad de Granada.html";
 const pathMetodos = "./data/Métodos Numéricos I _ Grado en Matemáticas.html";
 const pathMP = "./data/Metodología de la Programación _ Universidad de Granada.html";
+
+function leerHtmlTest(path: string): string {
+    try {
+        return Deno.readTextFileSync(path);
+    } catch (e) {
+        throw new Error(`${ERRORES.ARCHIVO_NO_ENCONTRADO} -> ${path}`);
+    }
+}
