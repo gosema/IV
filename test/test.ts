23b931f8cb81331992083dd1d774ea9b6e9b02b1
gosema <gomezlaynez@gmail.com>
2025-12-13 15:54:06 +0100

    feat: #36 Validar con datos reales de asignaturas
    Para validar que funciona nuestra lógica de negocio debemos
    interpretar datos reales obtenidos de las fuentes de la UGR.
    Aquí se establece la ruta de las variables constantes de los
    archivos de datos con los que probaremos. Son necesarios que sean
    constante ya que no se prevee que cambie la ruta ni el nombre de los
    ficheros de datos.

--- test/test.ts
+++ test/test.ts
@@ -3,3 +3,7 @@ import { describe, it } from "testing/bdd";
 import { expect } from "testing/expect";
 import { extraerSemestre } from "../src/scraper_semestre.ts"
 import { ERRORES } from '../src/errores.ts';
+
+const pathCalculo = "./data/Cálculo _ Universidad de Granada.html";
+const pathMetodos = "./data/Métodos Numéricos I _ Grado en Matemáticas.html";
+const pathMP = "./data/Metodología de la Programación _ Universidad de Granada.html";
