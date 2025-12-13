1a262534d3785f912d33bc668f7e4b9e694c8d80
gosema <gomezlaynez@gmail.com>
2025-12-13 12:44:35 +0100

    fix: closes #33 La asignatura tiene varias clases
    Se edita la estructura del dominio del problema para que la asignatura
    tenga varias clases practicas. De esta forma, al igual que con las clase
    teoricas, el atributo de clases podrá contener una lista de varias clases
    como ocurre en la realidad.

--- src/asignatura.ts
+++ src/asignatura.ts
@@ -3,10 +3,10 @@ import { Clase } from "./clase.ts";
 class Asignatura {
     // Ejemplo: "https://www.ugr.es/estudiantes/grados/grado-ingenieria-informatica/calculo"
     url: string;
-    clasesPracticas: Map<string, Clase>;
+    clasesPracticas: Map<string, Clase[]>;
     clasesTeoricas: Clase[];
 
-    constructor(url: string, clasesPracticas: [string, Clase][], clasesTeoricas: Clase[]) {
+    constructor(url: string, clasesPracticas: [string, Clase[]][], clasesTeoricas: Clase[]) {
         this.url = url;
         this.clasesPracticas = new Map(clasesPracticas);
         this.clasesTeoricas = clasesTeoricas;
