4fc7e40d6aaa7bd09171b1d7ae824a55435f14ff
gosema <gomezlaynez@gmail.com>
2025-12-13 14:48:48 +0100

    feat: #36 Creación de función con expresión regular
    Creamos una función privada de scraper_semestre ya que solo
    será utilizada para detectar del texto html el texto correspondiente
    a los Semestres.
    Además para preparar los datos para su procesamiento, se pasa todo a mayuscula
    y se eliminan los espacios del principio y finales.

--- src/scraper_semestre.ts
+++ src/scraper_semestre.ts
@@ -1,1 +1,17 @@
 //archivo para funciones de scrapeado de la lógica de negocio
+
+/**
+     * Responsabilidad: Búsqueda en el HTML (Scraping puro).
+     * Busca la celda que contiene el texto del semestre.
+     * Busca la estructura: `<tr><th class="...">Semestre</th><td>Primero</td></tr>`
+     * La expresión regular busca la palabra "Semestre" en una celda de tabla,
+     * salta toda la información hasta `<td>` y extrae su contenido hasta `</td>`,
+     * ignorando espacios en blanco alrededor o saltos de línea.
+*/
+function obtenerTextoSemestreRaw(html: string): string {
+    const regexSemestre = /Semestre<\/th>[^]*?<td>\s*([\s\S]*?)\s*<\/td>/i;
+    const matchSemestre = regexSemestre.exec(html);
+
+    const contenido = matchSemestre[1] ? matchSemestre[1] : "";
+    return contenido.trim().toLowerCase();
+}
